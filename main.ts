90a3ddf78fb13d4017fe87e42d180631af81feb1
yunpruna <146179357+yunpruna@users.noreply.github.com>
2023-10-18 09:07:11 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,19 @@
+scene.setBackgroundColor(9)
+let mySprite = sprites.create(img`
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    f f f f f f f f f f f f f f f f 
+    e e e e e e e e e e e e e e e e 
+    d d d d 6 6 6 6 6 6 6 d d d d d 
+    c c c 6 f f f f f f f 6 c c c c 
+    b b 6 f f f f f f f f f 6 b b b 
+    a a 6 f f f f f f f f f 6 a a a 
+    9 9 6 f f f f f f f f f 6 9 9 9 
+    8 8 6 f f f f f f f f f 6 8 8 8 
+    7 7 6 f f f f f f f f f 6 7 7 7 
+    6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+    5 5 6 2 3 1 5 4 8 9 a b 6 5 5 5 
+    4 4 4 6 b d e f 9 2 9 6 4 4 4 4 
+    3 3 3 3 6 6 6 6 6 6 6 3 3 3 3 3 
+    2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 2 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    `, SpriteKind.Player)
